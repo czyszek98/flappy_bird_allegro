d08ea4092057d1b869ad37935710f35dce59edf7
Mateusz Czyszewski <mateusz.czyszewski@gmail.com>
2026-02-11 23:17:22 +0100

    Update readme

---
+++ build/CMakeFiles/FlappyBird.dir/link.d
@@ -0,0 +1,151 @@
+FlappyBird: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtbeginS.o \
+  CMakeFiles/FlappyBird.dir/src/main.cpp.o \
+  CMakeFiles/FlappyBird.dir/src/obiekty.cpp.o \
+  /usr/lib/liballeg.so \
+  /usr/lib/libstdc++.so \
+  /usr/lib/libm.so \
+  /usr/lib/libm.so \
+  /usr/lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/libc.so \
+  /usr/lib/libc.so \
+  /usr/lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crtn.o \
+  /usr/lib32/libX11.so.6 \
+  /usr/lib/libX11.so.6 \
+  /usr/lib32/libXext.so.6 \
+  /usr/lib/libXext.so.6 \
+  /usr/lib/libXcursor.so.1 \
+  /usr/lib/libXpm.so.4 \
+  /usr/lib32/libXxf86vm.so.1 \
+  /usr/lib/libXxf86vm.so.1 \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib32/libxcb.so.1 \
+  /usr/lib/libxcb.so.1 \
+  /usr/lib/libXrender.so.1 \
+  /usr/lib32/libXfixes.so.3 \
+  /usr/lib/libXfixes.so.3 \
+  /usr/lib32/libXau.so.6 \
+  /usr/lib/libXau.so.6 \
+  /usr/lib32/libXdmcp.so.6 \
+  /usr/lib/libXdmcp.so.6
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtbeginS.o:
+
+CMakeFiles/FlappyBird.dir/src/main.cpp.o:
+
+CMakeFiles/FlappyBird.dir/src/obiekty.cpp.o:
+
+/usr/lib/liballeg.so:
+
+/usr/lib/libstdc++.so:
+
+/usr/lib/libm.so:
+
+/usr/lib/libm.so:
+
+/usr/lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/libc.so:
+
+/usr/lib/libc.so:
+
+/usr/lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crtn.o:
+
+/usr/lib32/libX11.so.6:
+
+/usr/lib/libX11.so.6:
+
+/usr/lib32/libXext.so.6:
+
+/usr/lib/libXext.so.6:
+
+/usr/lib/libXcursor.so.1:
+
+/usr/lib/libXpm.so.4:
+
+/usr/lib32/libXxf86vm.so.1:
+
+/usr/lib/libXxf86vm.so.1:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib32/libxcb.so.1:
+
+/usr/lib/libxcb.so.1:
+
+/usr/lib/libXrender.so.1:
+
+/usr/lib32/libXfixes.so.3:
+
+/usr/lib/libXfixes.so.3:
+
+/usr/lib32/libXau.so.6:
+
+/usr/lib/libXau.so.6:
+
+/usr/lib32/libXdmcp.so.6:
+
+/usr/lib/libXdmcp.so.6:
